1b09a08d265a0cf496fa870f128e1e93665a955e
PAVLO <pasha92@ukr.net>
2016-07-15 15:56:30 +0300

    '4'

---
+++ migrations/m160715_125351_create_config_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation for table `config`.
+ */
+class m160715_125351_create_config_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('config', [
+            'id' => $this->primaryKey(),
+            'title' => Schema::TYPE_STRING . '(255) NOT NULL',
+            'value' => Schema::TYPE_STRING . '(10000) NOT NULL',
+            'param_desc' => Schema::TYPE_STRING . '(10000) NOT NULL',
+            'valid' => Schema::TYPE_STRING . '(10000) NOT NULL',
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('config');
+    }
+}
